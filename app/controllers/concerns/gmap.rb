7f8bd43a404c488ed301e9f9748d6a1e0ed66bf1
punkshiraishi <pharmacy.sy@gmail.com>
2019-11-24 17:04:47 +0900

    add layout micropost on map

--- app/controllers/concerns/gmap.rb
+++ app/controllers/concerns/gmap.rb
@@ -2,15 +2,16 @@ module Gmap
   extend ActiveSupport::Concern
 
   included do
-
     def build_markers(microposts)
       Gmaps4rails.build_markers(microposts) do |micropost, marker|
         marker.lat micropost.latitude
         marker.lng micropost.longitude
-        marker.infowindow micropost.content
-        marker.json({ picture: { url: ApplicationController.helpers.gravatar_url(micropost.user, 32), width: 32, height: 32 } })
+        # marker.infowindow "<div class='map_user_name'>#{micropost.user.name}</div>
+        #                    <br>欲しい物資：#{micropost.content}
+        #                    <br>#{micropost.created_at}"
+        marker.infowindow render_to_string(partial: 'microposts/micropost_in_map', locals: {micropost: micropost})
+        marker.picture url: ApplicationController.helpers.gravatar_url(micropost.user, 32), width: 32, height: 32
       end
     end
-
   end
 end
