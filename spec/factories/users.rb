14e35b111d850f33b2b46716e4115f76d5ffe5a3
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-16 19:56:12 +0900

    :rotating_light: Add user factory bot

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name {"Mikite"}
+    email {"mikite@gmail.com"}
+    password {"mikitepassword"}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is valid with a name, email, and password" do
-    user = User.new(
-      name: "Mikite",
-      email: "mikite@gmail.com",
-      password: "mikitepassword"
-    )
-    expect(user).to be_valid
+    expect(FactoryBot.build(:user)).to be_valid
   end
 
   it "is invalid without a name" do
